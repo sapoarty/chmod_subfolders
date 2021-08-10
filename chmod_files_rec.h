456450eea98219a03ebc13ba1e680b68e9ffdbb8
sapoarty <87600990+sapoarty@users.noreply.github.com>
2021-08-10 19:10:19 +0300

    Makefile added

--- chmod_files_rec.h
+++ chmod_files_rec.h
@@ -5,7 +5,7 @@ void chmod_files_rec(char *basePath);
 
 #define PATH_MAX_LNG                        256
 #define FILE_PERMISSIONS                    (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-#define FOLDER_PERMISSIONS                    (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)
+#define FOLDER_PERMISSIONS                  (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)
 #define PATH_REQUEST_MSG                    "Path to list files (\".\" to use current folder): "
 #define SUCESS_MSG                          "All file permissions have been changed\n"
 #define FAIL_MSG                            "Directory doesn't exist\n"
